121b86bd489ebc7ad4a8631fe017a38c3d158070
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-09-09 14:13:46 +0800

    submit for the first subtask

--- 10283.c
+++ 10283.c
@@ -5,12 +5,16 @@ int main()
   int digit;
   int number = 0;
   scanf("%d", &digit);
+#ifdef DEBUG
   printf("%d\n", digit);
+#endif
   while (digit != 0) {
     if (digit >= 1 && digit <= 7)
       number = number * 10 + digit;
     scanf("%d", &digit);
+#ifdef DEBUG
     printf("%d\n", digit);
+#endif
   }
   printf("%d\n", number);
   return 0;
