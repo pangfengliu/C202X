bfb874707f05d11d7dc0f403f776fc87f48fac44
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-09-09 14:30:04 +0800

    multiplication and addition

--- 10283.c
+++ 10283.c
@@ -5,7 +5,8 @@ int main()
   int digit;
   int number = 0;
   int product = 1;
-  
+  int sum = 0;
+    
   scanf("%d", &digit);
 #ifdef DEBUG
   printf("%d\n", digit);
@@ -14,17 +15,22 @@ int main()
   while (digit != 0) {
     if (digit >= 1 && digit <= 7)
       number = number * 10 + digit;
-    else {			/* 8 */
+    else {			/* 8 or 9 */
       product *= number;
+      if (digit == 8) {
+	sum += product;
+	product = 1;
+      }
       number = 0;
     }
-    
+
     scanf("%d", &digit);
 #ifdef DEBUG
     printf("%d\n", digit);
 #endif
   }
   product *= number;		/* the last one */
-  printf("%d\n", product);
+  sum += product;
+  printf("%d\n", sum);
   return 0;
 }
