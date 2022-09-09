000fbcebb6310f272b40d5e81805bf02fa3d0467
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-09-09 14:11:14 +0800

    compute number OK

--- 10283.c
+++ 10283.c
@@ -3,11 +3,15 @@
 int main()
 {
   int digit;
+  int number = 0;
   scanf("%d", &digit);
   printf("%d\n", digit);
   while (digit != 0) {
+    if (digit >= 1 && digit <= 7)
+      number = number * 10 + digit;
     scanf("%d", &digit);
     printf("%d\n", digit);
   }
+  printf("%d\n", number);
   return 0;
 }
