4ff0d918a9797d786c877f47c06a4daded893e75
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-09-09 14:23:39 +0800

    multiplication only

--- 10283.c
+++ 10283.c
@@ -4,18 +4,27 @@ int main()
 {
   int digit;
   int number = 0;
+  int product = 1;
+  
   scanf("%d", &digit);
 #ifdef DEBUG
   printf("%d\n", digit);
 #endif
+
   while (digit != 0) {
     if (digit >= 1 && digit <= 7)
       number = number * 10 + digit;
+    else {			/* 8 */
+      product *= number;
+      number = 0;
+    }
+    
     scanf("%d", &digit);
 #ifdef DEBUG
     printf("%d\n", digit);
 #endif
   }
-  printf("%d\n", number);
+  product *= number;		/* the last one */
+  printf("%d\n", product);
   return 0;
 }
