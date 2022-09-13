b0b3c47f051532720ffe9e4b1967e4b00e27c199
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-09-13 11:48:08 +0800

    the second digit

--- 10284.c
+++ 10284.c
@@ -2,7 +2,10 @@
 
 int main()
 {
+  int k;
+  scanf("%d", &k);
   for (int i = 1; i <= 9; i++)
-    printf("%d\n", i);
+    for (int j = ((i - k) < 0? 0 : i - k); j <= (i + k > 9? 9 : i + k); j++)
+      printf("%d%d\n", i, j);
   return 0;
 }
