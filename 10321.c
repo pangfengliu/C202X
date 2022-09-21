b997bff35e06ac1dfb14c4c21e4f2f17e770b156
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-09-20 20:32:52 -0700

    compute the lcm of a and b

--- 10321.c
+++ 10321.c
@@ -3,8 +3,32 @@
 int main()
 {
   int a, b, c, d;
-  while (scanf("%d%d%d%d", &a, &b, &c, &d) != EOF)
+  while (scanf("%d%d%d%d", &a, &b, &c, &d) != EOF) {
     printf("%d %d %d %d\n", a, b, c, d);
 
+    int ablcm;
+    {
+      int m = a;
+      int n = b;
+      int x, y;
+      
+      if (m > n) {
+	x = m;
+	y = n;
+      } else {
+	x = n;
+	y = m;
+      }
+      
+      while (x % y != 0) {
+	int remainder = x % y;
+	x = y;
+	y = remainder;
+      }
+      ablcm = m * n / y;
+    }
+    printf("ablcm = %d\n", ablcm);
+  }
+  
   return 0;
 }
