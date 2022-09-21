26f258fa1e3a11157869bf3eb44ac5cc47008fd8
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-09-20 21:09:02 -0700

    use function

--- 10321.c
+++ 10321.c
@@ -1,87 +1,33 @@
 #include <stdio.h>
 
+int lcm(int m, int n)
+{
+  int x, y;
+  
+  if (m > n) {
+    x = m;
+    y = n;
+  } else {
+    x = n;
+    y = m;
+  }
+  
+  while (x % y != 0) {
+    int remainder = x % y;
+    x = y;
+    y = remainder;
+  }
+  return (m * n / y);
+}
+
 int main()
 {
   int a, b, c, d;
   while (scanf("%d%d%d%d", &a, &b, &c, &d) != EOF) {
-#ifdef DEBUG
-    printf("%d %d %d %d\n", a, b, c, d);
-#endif
-    int ablcm;
-    {
-      int m = a;
-      int n = b;
-      int x, y;
-      
-      if (m > n) {
-	x = m;
-	y = n;
-      } else {
-	x = n;
-	y = m;
-      }
-      
-      while (x % y != 0) {
-	int remainder = x % y;
-	x = y;
-	y = remainder;
-      }
-      ablcm = m * n / y;
-    }
-#ifdef DEBUG
-    printf("ablcm = %d\n", ablcm);
-#endif
-    int abclcm;
-    {
-      int m = ablcm;
-      int n = c;
-      int x, y;
-      
-      if (m > n) {
-	x = m;
-	y = n;
-      } else {
-	x = n;
-	y = m;
-      }
-      
-      while (x % y != 0) {
-	int remainder = x % y;
-	x = y;
-	y = remainder;
-      }
-      abclcm = m * n / y;
-    }
-#ifdef DEBUG
-    printf("abclcm = %d\n", abclcm);
-#endif
-  
-    int abcdlcm;
-    {
-      int m = abclcm;
-      int n = d;
-      int x, y;
-      
-      if (m > n) {
-	x = m;
-	y = n;
-      } else {
-	x = n;
-	y = m;
-      }
-      
-      while (x % y != 0) {
-	int remainder = x % y;
-	x = y;
-	y = remainder;
-      }
-      abcdlcm = m * n / y;
-    }
-#ifdef DEBUG
-    printf("abcdlcm = %d\n", abcdlcm);
-#endif
+    int ablcm = lcm(a, b);
+    int abclcm = lcm(ablcm, c);
+    int abcdlcm = lcm(abclcm, d);
     printf("%d\n", abcdlcm / a);
   }
-
   return 0;
 }
