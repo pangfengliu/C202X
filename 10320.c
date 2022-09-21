ef6a9fce621f95febe43fac559b78ecf0b4ea1d2
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-09-21 04:28:34 -0700

    print the solution

--- 10320.c
+++ 10320.c
@@ -13,15 +13,25 @@ int main()
     
   int index = 1;
   for (int round = 1; round <= R; round++) {
-    int row = round;
     int width = M - round + 1;
+#ifdef DEBUG
     printf("row %d from %d to %d\n", round, index, index + width - 1);
+#endif
+    if (k >= index && k < index + width) {
+      printf("%d %d\n", round, k - index + round);
+      return 0;
+    }
     index += width;
 
     if (round < N) {
-      int col = round;
       int height = N - round;
+#ifdef DEBUG
       printf("col %d from %d to %d\n", round, index, index + height - 1);
+#endif
+      if (k >= index && k < index + height) {
+	printf("%d %d\n", k - index + round + 1, round);
+	return 0;
+      }
       index += height;
     }
   }
