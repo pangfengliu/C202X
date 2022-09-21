a12482bad36abeb426b752b9e5722d07a3f31290
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-09-21 02:49:16 -0700

    fix the round number and the last column

--- 10320.c
+++ 10320.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
+int min(int a, int b)
+{
+  return (a < b? a : b);
+}
+  
 int main()
 {
   int N, M, k;
   scanf("%d%d%d", &N, &M, &k);
-
+  int R = min(M, N);
+    
   int index = 1;
-  for (int round = 1; round <= N; round++) {
+  for (int round = 1; round <= R; round++) {
     int row = round;
     int width = M - round + 1;
     printf("row %d from %d to %d\n", round, index, index + width - 1);
     index += width;
 
-    int col = round;
-    int height = N - round;
-    printf("col %d from %d to %d\n", round, index, index + height - 1);
-    index += height;
+    if (round < N) {
+      int col = round;
+      int height = N - round;
+      printf("col %d from %d to %d\n", round, index, index + height - 1);
+      index += height;
+    }
   }
   return 0;
 }
