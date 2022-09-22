8381efcd84c61ee53e2d5bf403a42129376ec31e
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-09-21 20:27:57 -0700

    start with rook attack

---
+++ 50200.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#define MAXR 500
+#define MAXC 500
+#define DIR 4
+
+#define STONE 0
+#define QUEEN 1
+#define BISHOP 2
+#define ROOK 3
+
+bool in(int row, int col, int R, int C)
+{
+  return (row >= 0 && row < R && col >= 0 && col < C);
+}
+
+void attack(bool stone[MAXR][MAXC], int row, int col, int dr, int dc, int R, int C)
+{
+  int r = row + dr;
+  int c = col + dc;
+  while (in(r, c, R, C) && !stone[r][c]) {
+    stone[r][c] = true;
+    r += dr;
+    c += dc;
+  }
+}
+
+void rookAttack(bool stone[MAXR][MAXC], int row, int col, int R, int C)
+{
+  int dr[] = {0, 0, 1, -1};
+  int dc[] = {1, -1, 0, 0};
+  for (int i = 0; i < DIR; i++)
+    attack(stone, row, col, dr[i], dc[i], R, C);
+  stone[row][col] = true;
+}
+
+int main()
+{
+  int R, C;
+  assert(scanf("%d%d", &R, &C) == 2);
+
+  bool stone[MAXR][MAXC] = {{false}};
+  int type, row, col;
+  while (scanf("%d%d%d", &type, &col, &row) != EOF) {
+    switch (type) {
+    case STONE:
+      stone[row][col] = true;
+      break;
+    case ROOK:
+      rookAttack(stone, row, col, R, C);
+      break;
+    }
+  }
+
+  for (int r = 0; r < R; r++)
+    for (int c = 0; c < C; c++)
+      printf("%d%c", stone[r][c], (c == C - 1? '\n' : ' '));
+
+  return 0;
+}
