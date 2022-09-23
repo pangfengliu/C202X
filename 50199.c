dda6409527d643fccf50daec394bebf96b0449db
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-09-22 19:40:15 -0700

    read inputs

--- 50199.c
+++ 50199.c
@@ -14,6 +14,21 @@ void printCandy(int candy[MAXR][MAXC], int R, int C)
       printf("%d%c", candy[r][c], c == C - 1? '\n' : ' ');
 }
 
+void findSame(int candy[MAXR][MAXC], int row, int col, int R, int C)
+{
+}
+
+
+void remove3(int candy[MAXR][MAXC], int R, int C)
+{
+  for (int r = 0; r < R; r++)
+    for (int c = 0; c < C; c++)
+      if (candy[r][c] != 0) {
+	findSame(candy, r, c, R, C);
+      }
+}
+
+
 int main()
 {
   int R, C;
@@ -24,6 +39,7 @@ int main()
     for (int c = 0; c < C; c++)
       scanf("%d", &(candy[r][c]));
 
+  remove3(candy, R, C);
   printCandy(candy, R, C);
   return 0;
 }
