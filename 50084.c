52324991c063851da6a888ab404dacff3c339829
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-09-28 21:00:50 -0700

    submission for the first subtask for one machine

--- 50084.c
+++ 50084.c
@@ -15,7 +15,9 @@ int main()
   int M, J;
   assert(scanf("%d%d", &M, &J) == 2);
   assert(M == 1);
+#ifdef DEBUG
   printf("J %d\n", J);
+#endif
 
   int Jready[MAXJ];
   int numTask[MAXJ];
@@ -25,16 +27,22 @@ int main()
   for (int j = 0; j < J; j++) {
     assert(scanf("%d%d", &(Jready[j]), &(numTask[j])) == 2);
     assert(Jready[j] == 0);
+#ifdef DEBUG
     printf("ready %d num %d\n", Jready[j], numTask[j]);
+#endif
     for (int t = 0; t < numTask[j]; t++) {
       int machine;
       assert(scanf("%d%d", &machine, &time[j][t]) == 2);
       assert(machine == 0); /* a single machine */
+#ifdef DEBUG
       printf("time for task %d = %d\n", t, time[j][t]);
+#endif
     }
     taskTotal += numTask[j];
   }
+#ifdef DEBUG
   printf("total task = %d\n", taskTotal);
+#endif
 
   int currentTask[MAXJ] = {0};
   int Mready = 0;
@@ -51,7 +59,9 @@ int main()
       }
     }
     assert(ECjob != -1);
+#ifdef DEBUG
     printf("next task of job %d completes at %d\n", ECjob, ECtime);
+#endif
     Jready[ECjob] = ECtime;
     Mready = ECtime;
     currentTask[ECjob]++;
