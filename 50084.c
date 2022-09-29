dce98553cb5340b66cf7bd03da07cb6ea6fa6dfd
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-09-28 21:27:02 -0700

    submission for the second subtask for multiple machine

--- 50084.c
+++ 50084.c
@@ -3,6 +3,7 @@
 #include <limits.h>
  
 #define MAXJ 500
+#define MAXM 500
 #define MAXTPERJOB 500
 
 int max(int a, int b)
@@ -14,16 +15,16 @@ int main()
 {
   int M, J;
   assert(scanf("%d%d", &M, &J) == 2);
-  assert(M == 1);
 #ifdef DEBUG
-  printf("J %d\n", J);
+  printf("M %d J %d\n", M, J);
 #endif
 
-  int Jready[MAXJ];
+  int Jready[MAXJ];		/* job ready time */
   int numTask[MAXJ];
-  int time[MAXJ][MAXTPERJOB];
 
   int taskTotal = 0;
+  int machine[MAXJ][MAXTPERJOB];
+  int time[MAXJ][MAXTPERJOB];
   for (int j = 0; j < J; j++) {
     assert(scanf("%d%d", &(Jready[j]), &(numTask[j])) == 2);
     assert(Jready[j] == 0);
@@ -31,9 +32,7 @@ int main()
     printf("ready %d num %d\n", Jready[j], numTask[j]);
 #endif
     for (int t = 0; t < numTask[j]; t++) {
-      int machine;
-      assert(scanf("%d%d", &machine, &time[j][t]) == 2);
-      assert(machine == 0); /* a single machine */
+      assert(scanf("%d%d", &machine[j][t], &time[j][t]) == 2);
 #ifdef DEBUG
       printf("time for task %d = %d\n", t, time[j][t]);
 #endif
@@ -45,16 +44,21 @@ int main()
 #endif
 
   int currentTask[MAXJ] = {0};
-  int Mready = 0;
+  int Mready[MAXM] = {0};
   for (int t = 0; t < taskTotal; t++) {
     int ECtime = INT_MAX;
     int ECjob = -1;
+    int ECmachine;
     for (int j = 0; j < J; j++) {
       if (currentTask[j] < numTask[j]) { /* there are still tasks */
-	int completion = max(Mready, Jready[j]) + time[j][currentTask[j]];
+	int task = currentTask[j];
+	int completion =
+	  max(Mready[machine[j][task]], Jready[j]) +
+	  time[j][task];
 	if (completion < ECtime) { /* choose the smaller index */
 	  ECtime = completion;
 	  ECjob = j;
+	  ECmachine = machine[j][task];
 	}
       }
     }
@@ -63,7 +67,7 @@ int main()
     printf("next task of job %d completes at %d\n", ECjob, ECtime);
 #endif
     Jready[ECjob] = ECtime;
-    Mready = ECtime;
+    Mready[ECmachine] = ECtime;
     currentTask[ECjob]++;
   }
   
