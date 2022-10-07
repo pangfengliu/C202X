84325eb28baa633aa78c9fb673a91f1c2f34a0db
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-10-06 23:12:56 -0700

    all subtasks

--- 50126.c
+++ 50126.c
@@ -4,16 +4,33 @@
 
 void merge(int A[], int B[], int* a[], int* b[])
 {
+#ifdef SUBTASK1
   assert(a[0] == NULL && b[0] == NULL); /* one ellment */
+#endif
   bool lastIsA;
   int **last = NULL;
   int indexA = 0, indexB = 0;
-  if (A[indexA] < B[indexB]) {
-    last = &(a[indexA]);
-    lastIsA = true;
-  } else {
-    last = &(b[indexB]);
-    lastIsA = false;
-  }
+
+  do {
+    if (A[indexA] < B[indexB]) {
+      if (last == NULL)
+	last = &(a[indexA]);
+      else {
+	(*last) = &(A[indexA]);
+	last = &(a[indexA]);
+      }
+      indexA++;
+      lastIsA = true;
+    } else {
+      if (last == NULL)
+	last = &(b[indexB]);
+      else {
+	(*last) = &(B[indexB]);
+	last = &(b[indexB]);
+      }
+      indexB++;
+      lastIsA = false;
+    }
+  } while (*last != NULL);
   *last = lastIsA? &(B[indexB]) : &(A[indexA]);
 }
