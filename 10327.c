30dec8100b64513e083b8bad612e45666e41f83d
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-10-14 00:07:28 -0700

    init heap

--- 10327.c
+++ 10327.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 
 #define MAXN 105
 #define MAXS (MAXN * MAXN)
@@ -15,7 +16,7 @@ void getTower(int N, int tower[MAXN][MAXN][MAXN])
   for (int layer = 1; layer <= N; layer++)
     for (int row = 0; row < layer; row++)
       for (int col = 0; col < layer; col++)
-	assert(scanf("%d", &(tower[row][col][N - layer - 1])) == 1);
+	assert(scanf("%d", &(tower[row][col][N - layer])) == 1);
 }
 
 void computeHeight(int N, int height[MAXN][MAXN])
@@ -25,6 +26,18 @@ void computeHeight(int N, int height[MAXN][MAXN])
       height[row][col] = N - max(row, col);
       printf("height [%d][%d] = %d\n", row, col, height[row][col]);
     }
+}
+
+void insert(int heap[MAXS][MAXC], int key, int S)
+{
+  int index = (77 * key + 2222) % S;
+  for (int i = 0; i < MAXC; i++)
+    if (heap[index][i] == 0) {
+      heap[index][i] = key;
+      return;
+    }
+  printf("no space found");
+  exit(-1);
 }
 
 int main()
@@ -40,9 +53,20 @@ int main()
 
   int height[MAXN][MAXN];
   computeHeight(N, height);
+
+  int heap[MAXS][MAXC] = {{0}}; 
+  for (int row = 0; row < N; row++)
+    for (int col = 0; col < N; col++) {
+      printf("insert %d into heap\n", tower[row][col][height[row][col] - 1]);
+      insert(heap, tower[row][col][height[row][col] - 1], S);
+    }
+
+  for (int s = 0; s < S; s++) {
+    for (int c = 0; c < C; c++)
+      if (heap[s][c] > 0)
+	printf("%d ", heap[s][c]);
+    printf("\n");
+  }
   
   return 0;
 }
-  
-
-      
