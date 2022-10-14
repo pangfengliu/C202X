6371eb638bf95ca870eca415a9cf2d9e36758548
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-10-13 23:51:24 -0700

    use functions

--- 10327.c
+++ 10327.c
@@ -10,6 +10,23 @@ int max(int a, int b)
   return (a > b? a : b);
 }
 
+void getTower(int N, int tower[MAXN][MAXN][MAXN])
+{
+  for (int layer = 1; layer <= N; layer++)
+    for (int row = 0; row < layer; row++)
+      for (int col = 0; col < layer; col++)
+	assert(scanf("%d", &(tower[row][col][N - layer - 1])) == 1);
+}
+
+void computeHeight(int N, int height[MAXN][MAXN])
+{
+  for (int row = 0; row < N; row++)
+    for (int col = 0; col < N; col++) {
+      height[row][col] = N - max(row, col);
+      printf("height [%d][%d] = %d\n", row, col, height[row][col]);
+    }
+}
+
 int main()
 {
   int N, S, C;
@@ -19,17 +36,10 @@ int main()
   assert(1 <= C && C <= MAXC);
 
   int tower[MAXN][MAXN][MAXN];
-  for (int layer = 1; layer <= N; layer++)
-    for (int row = 0; row < layer; row++)
-      for (int col = 0; col < layer; col++)
-	assert(scanf("%d", &(tower[row][col][N - layer - 1])) == 1);
+  getTower(N, tower);
 
   int height[MAXN][MAXN];
-  for (int row = 0; row < N; row++)
-    for (int col = 0; col < N; col++) {
-      height[row][col] = N - max(row, col);
-      printf("height [%d][%d] = %d\n", row, col, height[row][col]);
-    }
+  computeHeight(N, height);
   
   return 0;
 }
