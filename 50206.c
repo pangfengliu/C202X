47abe0142f5cce17793a82b4d560df9041123910
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-10-16 20:05:39 -0700

    compute frequency

--- 50206.c
+++ 50206.c
@@ -7,9 +7,23 @@
 int main()
 {
   char word[N + 1];
+  int frequency[L][L][L] = {{{0}}};
  
-  while (scanf("%s", word) != EOF) 
+  while (scanf("%s", word) != EOF) {
     puts(word);
+    int length = strlen(word);
+    if (length >= 3) {
+      for (int i = 0; i < length - 2; i++)
+	frequency[word[i] - 'a'][word[i + 1] - 'a'][word[i + 2] - 'a']++;
+    }
+  }
 
+  for (int i = 0; i < L; i++)
+    for (int j = 0; j < L; j++)
+      for (int k = 0; k < L; k++)
+	if (frequency[i][j][k] > 0)
+	  printf("frequency[%c][%c][%c] = %d\n",
+		 'a' + i, 'a' + j, 'a' + k, frequency[i][j][k]);
+    
   return 0;
 }
