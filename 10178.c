1c52691e770144da4c4c7d9f860eed8d20fbbf78
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-10-22 01:48:45 -0700

    sort frequency and keys

--- 10178.c
+++ 10178.c
@@ -17,6 +17,21 @@ int insert(int *ptr, int *ptrTable[], int object[], int frequency[],
   return currentCount + 1;
 }
 
+void sort(int frequency[], int object[], int count)
+{
+  for (int i = count - 2; i >= 0; i--)
+    for (int j = 0; j <= i; j++)
+      if (frequency[j] > frequency[j + 1] || 
+      (frequency[j] == frequency[j + 1] && object[j] >= object[j + 1])) {
+    int temp = frequency[j];
+    frequency[j] = frequency[j + 1];
+    frequency[j + 1] = temp;
+    temp = object[j];
+    object[j] = object[j + 1];
+    object[j + 1] = temp;
+      }
+}
+
 void count(int **p[])
 {
   int ***firstLevel = p;
@@ -31,6 +46,10 @@ void count(int **p[])
     printf("%d\n", *thirdLevel);
     firstLevel++;
   }
+  for (int i = 0; i < count; i++)
+    printf("%d %d\n", object[i], frequency[i]);
+
+  sort(frequency, object, count);
   for (int i = 0; i < count; i++)
     printf("%d %d\n", object[i], frequency[i]);
 }
