ebda5eef6b5b7bdfd8ef2b4f4dce02cf05360be2
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-10-22 01:44:26 -0700

    compute frequency and store key values

--- 10178.c
+++ 10178.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
 
+#define N 512
+
+int insert(int *ptr, int *ptrTable[], int object[], int frequency[], 
+	   int currentCount)
+{
+  for (int i = 0; i < currentCount; i++)
+    if (ptr == ptrTable[i]) {
+      frequency[i]++;
+      return currentCount;
+    }
+  /* not found */
+  ptrTable[currentCount] = ptr;
+  frequency[currentCount] = 1;
+  object[currentCount] = *ptr;
+  return currentCount + 1;
+}
+
 void count(int **p[])
 {
   int ***firstLevel = p;
+  int count = 0;
+  int frequency[N] = {0};
+  int object[N];
+  int *thirdLevelPtrTable[N];
+
   while (*firstLevel != NULL) {
-    int key = ***firstLevel;
-    printf("%d\n", key);
+    int *thirdLevel = **firstLevel;
+    count = insert(thirdLevel, thirdLevelPtrTable, object, frequency, count);
+    printf("%d\n", *thirdLevel);
     firstLevel++;
   }
+  for (int i = 0; i < count; i++)
+    printf("%d %d\n", object[i], frequency[i]);
 }
