e8d2d99d29abc747a5ba8868965d303e68531682
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-11-06 23:13:29 -0800

    use center of mass

--- 10186.c
+++ 10186.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
- 
+#include <assert.h>
+#include <stdint.h>
+
+#define COM
+
 int findSupport(int seesaw[], int start, int end)
 {
   if (end - start < 2)
     return -1;
- 
+#ifdef COM
+  int64_t moment = 0;
+  int64_t weight = 0;
+  for (int i = start; i <= end; i++) {
+    weight += seesaw[i];
+    moment += i * seesaw[i];
+  }
+  if (moment % weight == 0)
+    return moment / weight;
+#else
   for (int support = start; support <= end; support++) {
     /* check if balanced */
     long long int leftTorque = 0;
@@ -16,6 +29,7 @@ int findSupport(int seesaw[], int start, int end)
     if (leftTorque == rightTorque)
       return support;
   }
+#endif
   return -1;
 }
  
@@ -33,9 +47,10 @@ void findAllSupport(int seesaw[], int start, int end)
 int main()
 {
   int seesaw[17000];
-  int length; scanf("%d", &length);
+  int length;
+  assert(scanf("%d", &length) == 1);
   for (int i = 0; i < length; i++) {
-    scanf("%d", &(seesaw[i]));
+    assert(scanf("%d", &(seesaw[i])) == 1);
 #ifdef DEBUG
     printf("%d\n", seesaw[i]);
 #endif
