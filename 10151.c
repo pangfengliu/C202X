83524030186ab9c31f407c5a7e964510add615e7
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-11-18 01:01:35 -0800

    another recursive solution with cut

--- 10151.c
+++ 10151.c
@@ -9,30 +9,27 @@ int min(int a, int b)
 {
   return ((a < b)? a : b);
 }
- 
+
 int shortestCycle(int dist[MAXN][MAXN], bool visited[MAXN], int citiesLeft,
           int cost, int now, int N, int *best)
 {
-#ifdef DEBUG
-  printf("%d %d %d\n", citiesLeft, cost, now);
-#endif
-  if (citiesLeft == 0)
+  if (citiesLeft == 0) {
+    *best = min(*best, cost + dist[now][0]);   
     return(cost + dist[now][0]);
+  }
  
   if (cost >= *best)
     return INT_MAX;
  
   int minDistance = INT_MAX;
   for (int next = 1; next < N; next++)     /* 0 is visited */
-    if (!visited[next]) {
+    if (visited[next] == 0) {
       visited[next] = true;
       int distance = shortestCycle(dist, visited, citiesLeft - 1,
 				   cost + dist[now][next], next, N, best);
       minDistance = min(minDistance, distance);
       visited[next] = false;
     }
- 
-  *best = min(*best, minDistance);
   return minDistance;
 }
  
