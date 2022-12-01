7db06532ec6a39a38c5d51bb0d0c073f1aa22922
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-11-30 19:21:52 -0800

    with cut

--- 50246/50246.c
+++ 50246/50246.c
@@ -9,10 +9,15 @@ uint64_t min(uint64_t a, uint64_t b)
   
 uint64_t minSumSqu(const uint64_t number[], const int i, 
 		   uint64_t sum[], const uint64_t currentSum,
-		   const int K, const int N)
+		   const int K, const int N, uint64_t *best)
 {
-  if (i == N)
-    return currentSum;
+  if (i == N) {
+    *best = min(*best, currentSum);
+    return *best;
+  }
+
+  if (currentSum >= *best)
+    return UINT64_MAX;
 
   uint64_t minSum = UINT64_MAX;
   for (int g = 0; g < K; g++) {
@@ -21,7 +26,7 @@ uint64_t minSumSqu(const uint64_t number[], const int i,
     minSum =
       min(minSum, minSumSqu(number, i + 1, sum,
 			    currentSum + 2 * oldSum * number[i] + number[i] * number[i],
-			    K, N));
+			    K, N, best));
     sum[g] -= number[i];
   }
   return minSum;
@@ -41,6 +46,7 @@ int main()
     assert(scanf("%lu", &(number[i])) == 1);
 
   uint64_t sum[MAXN] = {0};
-  printf("%lu\n", minSumSqu(number, 0, sum, 0, K, N));
+  uint64_t best = UINT64_MAX;
+  printf("%lu\n", minSumSqu(number, 0, sum, 0, K, N, &best));
   return 0;
 }
