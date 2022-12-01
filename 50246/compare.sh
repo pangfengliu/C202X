14e071b38b47555c206964c1aa72d25342453433
Pangfeng Liu <pangfeng.liu@gmail.com>
2022-11-30 19:33:43 -0800

    all files

--- 50246/compare.sh
+++ 50246/compare.sh
@@ -1,3 +1,3 @@
-for i in {1..5} ; do ./50246 < $i.in > $i ; done
-for i in {1..5} ; do echo "comparing " 1-$i.out ; diff -Z $i.out $i ; done
+for i in {1..10} ; do ./50246 < $i.in > $i ; done
+for i in {1..10} ; do echo "comparing " 1-$i.out ; diff -Z $i.out $i ; done
 
