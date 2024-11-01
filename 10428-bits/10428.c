1dab0141816b0437d4127d00de7fe8007209e999
Pangfeng Liu <pangfeng.liu@gmail.com>
2024-11-01 11:43:19 +0800

    count row

--- 10428-bits/10428.c
+++ 10428-bits/10428.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <assert.h>
 
 #define W 64
+#define K 75000
 
 void getBits(uint64_t ull, bool bits[W])
 {
@@ -15,6 +17,27 @@ void getBits(uint64_t ull, bool bits[W])
   }
 }
 
+int countSeg(int l, int n)
+{
+  return (l >= n? l - n + 1 : 0);
+}
+
+int countRow(bool bits[W], int n)
+{
+  int count = 0;
+  int len = bits[0]? 1 : 0;
+  
+  for (int i = 1; i < W; i++)
+    if (bits[i])
+      len++;
+    else {
+      count += countSeg(len, n);
+      len = 0;
+    }
+  count += countSeg(len, n);
+  return count;
+}
+
 int main()
 {
   int n;
@@ -22,13 +45,14 @@ int main()
   bool bits[W];
   
   scanf("%d", &n);
-  scanf("%lu", &ull);
-
-  getBits(ull, bits);
-#ifdef DEBUG
-  for (int i = W - 1; i >= 0; i--)
-    printf("%d", bits[i] ? 1 : 0);
-  printf("\n");
-#endif
+  int row = 0;
+  
+  for (int i = 0; i < K; i++) {
+    assert(scanf("%lu", &ull) == 1);
+    getBits(ull, bits);
+    row += countRow(bits, n); 
+  }
+  printf("%d\n", row);
+  
   return 0;
 }
