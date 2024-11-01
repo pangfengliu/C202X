bcda626fa2c6dd99b0e7eafa4a0dd3aec36e4ff9
Pangfeng Liu <pangfeng.liu@gmail.com>
2024-11-01 12:38:32 +0800

    add antidiag

--- 10428-bits/10428.c
+++ 10428-bits/10428.c
@@ -42,7 +42,7 @@ int main()
   bool bits[W];
   
   scanf("%d", &n);
-  int row = 0, col = 0, colLen[W], diag = 0, diagLen[W];
+  int row = 0, col = 0, colLen[W], diag = 0, diagLen[W], antidiag = 0, antidiagLen[W];
   
   for (int r = 0; r < K; r++) {
     assert(scanf("%lu", &ull) == 1);
@@ -51,7 +51,7 @@ int main()
 
     if (r == 0) 
       for (int c = 0; c < W; c++) 
-	colLen[c] = diagLen[c] = (bits[c]? 1 : 0);
+	colLen[c] = diagLen[c] = antidiagLen[c] = (bits[c]? 1 : 0);
     else {
       /* col */
       for (int c = 0; c < W; c++) 
@@ -72,16 +72,29 @@ int main()
 	  diagLen[c] = 0;
 	}
       diagLen[W - 1] = bits[W - 1]? 1 : 0;
+
+      /* antidiag */
+      antidiag += countSeg(antidiagLen[W - 1], n);
+      for (int c = W - 1; c > 0; c--) 
+	if (bits[c])
+	  antidiagLen[c] = antidiagLen[c - 1] + 1;
+	else {
+	  antidiag += countSeg(antidiagLen[c - 1], n);
+	  antidiagLen[c] = 0;
+	}
+      antidiagLen[0] = bits[0]? 1 : 0;
     }
   }
   for (int c = 0; c < W; c++) {
     col += countSeg(colLen[c], n);
     diag += countSeg(diagLen[c], n);
+    antidiag += countSeg(antidiagLen[c], n);
   }
     
   printf("%d\n", row);
   printf("%d\n", col);
   printf("%d\n", diag);
+  printf("%d\n", antidiag);
   
   return 0;
 }
